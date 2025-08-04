0cb9229b8be6e25af4c725a9296cf1a466921feb
David Riosalido <driosalido@gmail.com>
2025-08-05 01:09:06 +0200

    Add existing Powerlevel10k configuration to dotfiles

--- dot_config/zsh/config/p10k.zsh
+++ dot_config/zsh/config/p10k.zsh
@@ -1,5 +1,4 @@
 # Powerlevel10k Configuration
-# This file will be replaced by the Powerlevel10k configuration wizard on first run
 # To reconfigure, run: p10k configure
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
@@ -9,5 +8,5 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-# To customize prompt, run `p10k configure` or edit ~/.config/zsh/config/p10k.zsh.
+# Source the main p10k configuration
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
